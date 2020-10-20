248bb21d948bcca03461b6dafe8a31288b656fbc
nah1222 <nah1222@yahoo.com>
2020-10-20 19:10:38 -0400

    hi

--- src/index.js
+++ src/index.js
@@ -1,24 +1,55 @@
 document.addEventListener('DOMContentLoaded', () => {
     const gameBaseUrl = "http://localhost:3000/api/v1/games/"
     const wordBaseUrl = "http://localhost:3000/api/v1/words/"
-    let phrases
+
+    let phrases = []
+
+    const fetchWords = () => {
+        fetch(wordBaseUrl)
+        .then(resp => resp.json())
+        .then(words => makeGame(words))
+    }
+
+    const makeGame = words => {
+        for(var i in words){
+            phrases.push(i, words, [i])
+        }
+    }
+    // const fetchWords = () => {
+    //     fetch(wordBaseUrl)
+    //     .then(resp => resp.json())
+    //     .then (words => {
+    //         phrases = words
+    //         makeGame()
+    //         // call a function that gets a random phrase from the phrases array 
+    //         // and renders it as a hangman game
+    //     });
+    // }
+   
+    
+
+    // let buttons = () => {
+    //     myButtons = document.getElementById('letterKey');
+    //     letters = document.createElement('ul');
+
+    //     for (let i = 0; i < allLetters.length; i++) {
+    //     // let alphabet = letters.id
+    //     const allLettersLi = document.createElement('li');
+    //     allLettersLi.classList.add = 'bet'
+    //         allLettersLi.textContent = allLetters[i];
+            
+    //         myButtons.appendChild(letters);
+    //         letters.appendChild(allLettersLi)
+
+    //     }   
+        
+    // }
 
 //     // const fetchGames = () => {
 //     //     fetch(gameBaseUrl)
 //     //     .then(response => response.json())
 //     //     .then(games => renderGames(games));
 //     // }
-
-    const fetchWords = () => {
-        fetch(wordBaseUrl)
-        .then(resp => resp.json())
-        .then (words => {
-            phrases = words
-            makeGame()
-            // call a function that gets a random phrase from the phrases array 
-            // and renders it as a hangman game
-        });
-    }
 
 //     const renderWords = (words) => {
 //         for(const word of words) {
@@ -37,7 +68,22 @@ document.addEventListener('DOMContentLoaded', () => {
 //     }
 
 
+fetchWords();
 
-// fetchWords();
-    
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
