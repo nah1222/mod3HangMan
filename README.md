eaa5de74b5061f2a099ac26d1110ee601d750a38
nah1222 <nah1222@yahoo.com>
2020-10-20 15:57:43 -0400

    Update

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # mod3HangMan
+
+1: Game functionality
+    1a. Create a box to act as a Letter Key and hold the already guessed letters
+    1b. Create or populate the hangman's noose
+    1c. Create a column that holds the hidden phrase. This column should populate letters based on a correct form submission.
+    1d. Create a form and submit button for the letter you plan to guess
+    1e. After this form is submitted, have the game respond with a conditional (If right, populate phrase, add letter to Letter Key, and reset form. Else, draw body part, populate Letter Key, and reset form)
+2: Create a start button
+    1a. Build out a form with a submit button
+    1b. Apply requirements (cannot submit without username. Stretch: Difficulty level)
+    1c. If successfully submitted, create a new game
+
--- src/index.js
+++ src/index.js
@@ -1,31 +1,43 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const gameBaseUrl = "http://loacalhost:3000/api/v1/games/"
-    const wordBaseUrl = "http://loacalhost:3000/api/v1/words/"
+    const gameBaseUrl = "http://localhost:3000/api/v1/games/"
+    const wordBaseUrl = "http://localhost:3000/api/v1/words/"
+    let phrases
 
-    // const fetchGames = () => {
-    //     fetch(gameBaseUrl)
-    //     .then(response => response.json())
-    //     .then(games => renderGames(games));
-    // }
+//     // const fetchGames = () => {
+//     //     fetch(gameBaseUrl)
+//     //     .then(response => response.json())
+//     //     .then(games => renderGames(games));
+//     // }
 
     const fetchWords = () => {
         fetch(wordBaseUrl)
         .then(resp => resp.json())
-        .then (words => renderWords(words));
+        .then (words => {
+            phrases = words
+            makeGame()
+            // call a function that gets a random phrase from the phrases array 
+            // and renders it as a hangman game
+        });
     }
 
-    const renderWords = (words) => {
-        for(const word of words) {
-            renderWord(word)
-        }
-    }
+//     const renderWords = (words) => {
+//         for(const word of words) {
+//             renderWord(word)
+//         }
+//     }
 
-    const renderWord = (word) => {
+//     const renderWord = (word) => {
+        
+//         const wordDiv = document.createElement('div')
+//         wordDiv.dataset.id = word.id
+//         wordDiv.innerHTML = `
+//         <h3> ${word.phrase} </h3>
+//         `
+//         document.body.appendChild(wordDiv)
+//     }
 
 
 
-
-    }
-
-
+// fetchWords();
+    
 })
